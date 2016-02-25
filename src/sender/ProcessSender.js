0cd34542990698d3d6fbecfeb14316e5fa5af6e1
MinkyuCho(Alan) <mrnoname@coupang.com>
2016-02-25 15:24:57 +0900

    Fix process.send bugs.

--- src/sender/ProcessSender.js
+++ src/sender/ProcessSender.js
@@ -4,10 +4,9 @@ import warning from '../warning';
 export default class ProcessSender extends Sender {
   constructor() {
     super();
-    this.sendMessage = process.send;
-    if (!this.sendMessage) {
+    if (!process.send) {
       warning('Process Sender only can work in clusters mode. This Process Sender will not send metricses.');
-      this.sendMessage = () => {
+      process.send = () => {
       };
     }
   }
@@ -15,7 +14,7 @@ export default class ProcessSender extends Sender {
   send(metrics) {
     return new Promise((resolve, reject) => {
       try {
-        this.sendMessage(metrics);
+        process.send(metrics);
         return resolve(metrics);
       } catch (error) {
         return reject(error);
